4d65993c3f0aac87eba6437187a93297c4cc18f6
Josh F <fowlerjosh93@gmail.com>
2019-09-11 11:42:23 -0400

    started array for questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+//create an array of objects for questions and answers
+var questions = [
+    {
+        question: "What is Charlie's favorite food?",
+        //separate array for answer choices
+        options: ["Cheese", "Fight Milk", "Milk Steak", "Sewage Crabs"],
+        answer: 2,
+        gif: "assets/images/ghouls.gif"
+    },
+    {
+        question: "Who is the Real Father of Dee and Dennis?",
+        //separate array for answer choices
+        options: ["Bruce Mathis", "Frank Reynolds", "Mr. Mac", "Doyle McPoyle"],
+        answer: 0,
+        gif: "assets/images/frank.gif"
+    },
+    {
+        question: "To Whom Does the Nightman Have to Pay a Toll to get to the Boy's Soul?",
+        //separate array for answer choices
+        options: ["Sweet Dee", "The Troll", "Bill Ponderosa", "Dayman"],
+        answer: 1,
+        gif: "assets/images/troll.gif"
+    },
+    {
+        question: "What is Charlie's Designation Within the Gang?",
+        //separate array for answer choices
+        options: ["The Looks", "The Brains", "The Big Cheese", "The Wild Card"],
+        answer: 3,
+        gif: "assets/images/wild.gif"
+    },
+    {
+        question: "What is Charlie's favorite food?",
+        //separate array for answer choices
+        options: ["Cheese", "Fight Milk", "Milk Steak", "Sewage Crabs"],
+        answer: 2,
+        gif: "assets/images/ghouls.gif"
+    },
+]
+
+
+//declare variables
+var time = 30;
+var 
